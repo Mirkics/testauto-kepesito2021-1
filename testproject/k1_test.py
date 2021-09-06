da69ff6b82f4d32832568185d6aadcc922ac9a64
Mirkics <eri_opre@yahoo.co.uk>
2021-09-06 13:32:18 +0200

    k1_test.py OK

--- testproject/k1_test.py
+++ testproject/k1_test.py
@@ -7,7 +7,7 @@
 # Az ellenőrzésekhez használj `pytest` keretrendszert. A tesztjeidben használj `assert` összehasonlításokat használj!"""
 #
 #
-# """* Helyesen jelenik meg az applikáció betöltéskor:
+# * Helyesen jelenik meg az applikáció betöltéskor:
 #     * a: <üres>
 #     * b: <üres>
 #     * c: <nem látszik>
@@ -21,7 +21,6 @@
 #     * a: <üres>
 #     * b: <üres>
 #     * c: NaN
-# """
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -37,4 +36,50 @@ driver.set_window_size(1000, 600, 600)
 URL = "https://ambitious-sky-0d3acbd03.azurestaticapps.net/k1.html"
 
 driver.get(URL)
+time.sleep(2)
+
+input_a = driver.find_element_by_id("a")
+input_b = driver.find_element_by_id("b")
+
+
+submit_button = driver.find_element_by_id("submit")
+result_text = driver.find_element_by_id("result")
+
+test_data = ["2", "3", ""]
+reference_data = ["10", "NaN"]
+
+
+#* Helyesen jelenik meg az applikáció betöltéskor:
+#     * a: <üres>
+#     * b: <üres>
+#     * c: <nem látszik>
+def correct_app_visible_test():
+    assert input_a != ""
+    assert input_b != ""
+
+print(input_a, input_b)
+
+# * Üres kitöltés:
+#     * a: <üres>
+#     * b: <üres>
+#     * c: NaN
+def empty_input_field_test():
+    submit_button.click()
+    assert result_text.text == reference_data[1]
+
+# * Számítás helyes, megfelelő bemenettel
+#     * a: 2
+#     * b: 3
+#     * c: 10
+def correct_output_test():
+    submit_button.click(test_data[0][1])
+    assert result_text.text == reference_data[0]
+
+
+#print(result_text.text)
+
+driver.quit()
+
+
+
 
